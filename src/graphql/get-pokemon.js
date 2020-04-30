a3a4455d1d66f4492484abf76ef208b433ce6ffa
Glitch (cool-pokemon) <none>
2020-04-30 07:43:46 +0000

    🏕🐉 Updated with Glitch

---
+++ src/graphql/get-pokemon.js
@@ -0,0 +1,35 @@
+import gql from 'graphql-tag'
+
+export const GET_POKEMONS = gql`
+  query pokemons($first: Int!) {
+    pokemons(first: $first){
+      id
+      name
+      image
+      maxHP
+      maxCP
+      attacks {
+        special {
+          name
+          damage
+        }
+      }
+    }
+  }
+`
+
+export const GET_POKEMON_NAME = gql`
+  query pokemon($id: String, $name: String!) {
+    pokemon(id: $id, name: name) {
+      image
+      maxHP
+      maxCP
+      attacks {
+        special {
+          name
+          damage
+        }
+      }
+    }
+  }
+`
